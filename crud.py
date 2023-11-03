42db4bf8d0b1b5b903899477a605c69dee215fdc
Renzo Lemmens <renzo@nas24.be>
2023-11-03 17:05:29 +0100

    Created crud.py

---
+++ crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+
+def get_blogposts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.BlogPost).offset(skip).limit(limit).all()
+
+def get_blogpost_by_id(db: Session, id: int):
+    return db.query(models.BlogPost).filter(models.BlogPost.id == id).first()
+
+def create_blog_post(db: Session, title: str, content: str, date: str):
+    db_post = BlogPost(title=title, content=content, date=date)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
