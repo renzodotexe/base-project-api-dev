b28f7206a1ff0ab8fa74ff55ddd071ff6c639599
Renzo Lemmens <renzo@nas24.be>
2023-11-06 19:39:11 +0100

    Moved python files to app folder to apply a structured hierarchy

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ import schemas
 from database import SessionLocal, engine
 import os
 
-if not os.path.exists('.\sqlitedb'):
-    os.makedirs('.\sqlitedb')
+if not os.path.exists('../sqlitedb'):
+    os.makedirs('../sqlitedb')
 
 #"sqlite:///./sqlitedb/sqlitedata.db"
 models.Base.metadata.create_all(bind=engine)
